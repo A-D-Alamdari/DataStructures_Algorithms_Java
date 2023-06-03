7ce143241c36aaba97d22118210630bc9dc5e939
ada <amin_deldari@hotmail.com>
2023-06-03 19:03:14 +0300

    Sorting Algorithms are added. Bubble Sort, Insertion Sort, Merge Sort, Selection Sort and Quick Sort.

---
+++ Java_DS_Algorithms/src/SortingAlgorithms/BubbleSort.java
@@ -0,0 +1,44 @@
+package SortingAlgorithms;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class BubbleSort {
+    /**
+     * Bubble Sort Algorithm. Sorting an array in place.
+     * @param array : The array to be sorted.
+     */
+    public static void bubbleSort(int[] array) {
+        for (int i = array.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = new int[100000];
+        for (int i = 0; i < 100000; i++) {
+            arr[i] = i;
+        }
+        Random rand = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            int randomIndexToSwap = rand.nextInt(arr.length);
+            int temp = arr[randomIndexToSwap];
+            arr[randomIndexToSwap] = arr[i];
+            arr[i] = temp;
+        }
+
+
+
+        long start = System.currentTimeMillis();
+        bubbleSort(arr);
+        long end = System.currentTimeMillis();
+        System.out.println("Computation time of Insertion Sort: " + (end - start));
+    }
+}
---
+++ Java_DS_Algorithms/src/SortingAlgorithms/InsertionSort.java
@@ -0,0 +1,46 @@
+package SortingAlgorithms;
+
+import java.util.Arrays;
+import java.util.Random;
+
+
+public class InsertionSort {
+    /**
+     * Insertion Sorting Algorithm. Sorting an array in place.
+     * @param array : The array to be sorted.
+     */
+    public static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j = i - 1;
+            while (j > -1 && temp < array[j]) {
+                array[j + 1] = array[j];
+                array[j] = temp;
+                j--;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[100000];
+        for (int i = 0; i < 100000; i++) {
+            arr[i] = i;
+        }
+        Random rand = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            int randomIndexToSwap = rand.nextInt(arr.length);
+            int temp = arr[randomIndexToSwap];
+            arr[randomIndexToSwap] = arr[i];
+            arr[i] = temp;
+        }
+
+
+
+        long start = System.currentTimeMillis();
+        insertionSort(arr);
+        long end = System.currentTimeMillis();
+        System.out.println("Computation time of Insertion Sort: " + (end - start));
+    }
+}
